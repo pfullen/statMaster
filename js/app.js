cdb9b2be867f83df7f015241ad310a6a34d758b0
pete <pete@localhost.localdomain>
2017-08-23 22:38:33 -0400

    Added index

--- js/app.js
+++ js/app.js
@@ -1,26 +1,32 @@
 'use strict';
 
 angular.module('myApp', [])
-  .controller('MovieController', function($scope, $http){
+  .controller('StatController', function($scope, $http){
     $scope.$watch('search', function() {
       fetch();
+
+       console.log($scope.details);
     });
 
-    $scope.search = "Sherlock Holmes";
+    $scope.search = "NY Mets";
 
     function fetch(){
-      $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+    /*  $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
       .then(function(response){ $scope.details = response.data; });
 
-      $http.get("http://www.omdbapi.com/?s=" + $scope.search)
+      $http.get("http://www.omdbapi.com/?s=" + $scope.search)http://api.sportradar.us/mlb-t6/players/6e1cac5c-b059-4b80-a267-5143b19efb27/profile.json?api_key=rsg2ehj4293rcr9x794dv7zr
       .then(function(response){ $scope.related = response.data; });
+      */
+      $http.get("http://api.sportradar.us/mlb-t6/players/6e1cac5c-b059-4b80-a267-5143b19efb27/profile.json?api_key=rsg2ehj4293rcr9x794dv7zr")
+       .then(function(response){ $scope.details = response.data; });
+
     }
 
-    $scope.update = function(movie){
-      $scope.search = movie.Title;
-    };
+//    $scope.update = function(movie){
+  //    $scope.search = movie.Title;
+  //  };
 
     $scope.select = function(){
-      this.setSelectionRange(0, this.value.length);
+   //   this.setSelectionRange(0, this.value.length);
     }
   });
